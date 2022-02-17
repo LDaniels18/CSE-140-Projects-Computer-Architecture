7e7fa5ac6646c669566296be097790f351b7e290
ldaniels18 <lafrancedanielsiii@hotmail.com>
2022-02-17 15:02:44 -0800

    Adding some more comments and testing out some C

--- HW3/LaFrance_Iveth.c
+++ HW3/LaFrance_Iveth.c
@@ -3,4 +3,21 @@
 //CSE 140 - Spring 2022
 //University of California - Merced
 
-//some more test comments
+/*
+instructions if using linux... to run you have to compile using gcc. 
+use: gcc LaFrance_Iveth.c --this compiles the program (only use if you dont want to name an executable file)
+next we have to create an executable file to show output:
+use: gcc LaFrance_Iveth.c -o LaFrance_Iveth (this is prefered and make sure to use everytime you add or delete code)
+finally we have to run the program using the executable file
+use: ./LaFrance_Iveth (always use after compiling to see output)
+*/
+
+#include<stdio.h>
+
+
+int main(){
+
+    printf("Hello \n");
+
+    return 0;
+}
