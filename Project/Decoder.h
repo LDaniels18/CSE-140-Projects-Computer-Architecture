f70c22873ad403985a9b82ab87bbfd38fe5d40ab
LDaniels18 <lafrancedanielsiii@hotmail.com>
2022-04-07 13:17:22 -0700

    Fetch works as intended, now fixing Decoder

--- Project/Decoder.h
+++ Project/Decoder.h
@@ -3,9 +3,9 @@
 
 //int foo(int x);  /* An example function declaration */
 
-//void Decode(); //function to actually decode
-int registers(char); // declaring the helper function by passing the instruction char array
-int binToDec(char);   // a function to change binary to dec (of any size)
+void Decode(char instruction); //function to actually decode
+int registers(char binCode[5]); // declaring the helper function by passing the instruction char array
+int binToDec(char binCode[]);   // a function to change binary to dec (of any size)
 
 
 #endif // FOO_H_
