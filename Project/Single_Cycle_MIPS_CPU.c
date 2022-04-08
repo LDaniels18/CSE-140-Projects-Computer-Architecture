efdad79fd71d5e9c09d7fb893f2585838c5e378a
ldaniels18 <lafrancedanielsiii@hotmail.com>
2022-04-07 22:37:15 -0700

    Making some updates for Fectch, adding PC

--- Project/Single_Cycle_MIPS_CPU.c
+++ Project/Single_Cycle_MIPS_CPU.c
@@ -29,33 +29,41 @@ For the Fetch Function (While Loop Process): https://stackoverflow.com/questions
 #define MAX_LENGTH 256 // using a length variable to help with reading the line of text
 
 // global variables:
-int PC = 0; // Starting Instruction
+int PC = 0;                                        // Starting Instruction
+char *instruction_counter[MAX_LENGTH][MAX_LENGTH]; // to act as a container pointing to an instruction
+// char *instruction[MAX_LENGTH];                     // a universal pointer for instructions
+int i = 0; // starting location in the instruction counter:
 
 // never changing filename (obviously change when you recieve a finalized file):
-const char *filename = "1_ins.txt";
+const char *filename = "sample_ins.txt";
 
-char CPU_Process();
-//void Decode(char instruction);
+void CPU_Process(); // to act as the main CPU function
+int fetch();        // to fetch instructions for the PC counter
 
 int main()
 {
-    //char instruction_catcher;
-
     printf("Welcome to our Single Cycle MIPS CPU!\n");
     // char instruction_catch; //will catch the results after fetch() is called (will be considered int in output)
 
-    printf("Preparing to fectch the 32 bit instruction....\n");
-    printf("\n");
     CPU_Process();
 
-    return 0; //ending the program
+    return 0; // ending the program
 
 } // end of main function
 
-char CPU_Process()
+void CPU_Process()
 {
-    // // we need a bit string (character array) to carry the instruction:
+    printf("Preparing to fectch the 32 bit instruction....\n");
+    printf("\n");
 
+    fetch(PC);
+
+} // end of CPU Process Function
+
+// A function to both fetch and decode a given instruction:
+int fetch()
+{
+    // we need a bit string (character array) to carry the instruction:
     char instruction[MAX_LENGTH]; // to carry the new 32 bit instruction
 
     printf("Fetching the Instruction...\n");
@@ -63,7 +71,7 @@ char CPU_Process()
     // creating an File Pointer object:
     FILE *file_accessor; // object
 
-    file_accessor = fopen("1_ins.txt", "r"); // using the fopen function to read from a file
+    file_accessor = fopen(filename, "r"); // using the fopen function to read from a file
 
     // CONDITIONS:
     if (file_accessor == NULL) // if theres nothing in the text file then we cant continue
@@ -77,29 +85,32 @@ char CPU_Process()
 
     // to read the file, line by line and add the contents to our carry variable
     while (fgets(instruction, MAX_LENGTH, file_accessor))
-    {
-        //indicating that instruction was fetched:
-         printf("Instruction Fetched!\n");
+    { // going to the line
+
+        // indicating that instruction was fetched:
+        printf("\n");
+        printf("Instruction Fetched!\n");
 
         // to get rid of the newline (for each line):
         instruction[strcspn(instruction, "\n")] = 0;
 
-        // to print the instruction bit by bit:
-        printf("Your 32 bit instruction was: %s\n", instruction);
+        instruction_counter[i][32] = instruction; // the instruction gets loaded at a particular location 4 spaces from one another or PC(4)
+
+        // to print the instruction bit by bit -- tests:
+        // printf("Your 32 bit instruction was: %s\n", instruction_counter[i][32]);
+        // printf("\n");
+        // printf("Instruction counter position: %d\n", i);
+        
         printf("\n");
+        printf("Decoding the Instruction.....");
+        Decode(instruction_counter[PC][32]);
+
+        i = i + 4;   // updating the index by 4 to place it in the instruction counter
+        PC = PC + 4; // always update the PC everytime you read an instruction
 
     } // end of while
 
-   
     fclose(file_accessor); // closing the read file (will need to be controlled by the PC)
 
-    //Running Decoder from Fetch:
-    printf("Preparing to Decode the Instruction...\n");
-    Decode(instruction);
-
-    return *instruction; // returning the value of instruction which is also going to be a int when used... need to fix to become a string
-
-    // next instruction to be pointed to by the Program Counter:
-    // int next_PC = PC + 4;
-
-} // end of fetch function
+    return 0; //ending the instruction
+} // end of fetch
